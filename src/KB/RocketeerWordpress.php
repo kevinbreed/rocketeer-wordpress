8112b2c532e30d6fe726582196f282cc81558a65
Kevin Breed <me@kevinbreed.nl>
2015-04-20 12:29:01 +0200

    Updated config

--- src/KB/RocketeerWordpress.php
+++ src/KB/RocketeerWordpress.php
@@ -1,18 +1,49 @@
 <?php
 
-namespace KB;
+namespace Rocketeer\Plugins\Wordpress;
 
 use Illuminate\Container\Container;
-use Rocketeer\Services\TasksHandler;
 use Rocketeer\Abstracts\AbstractPlugin;
+use Rocketeer\Facades\Rocketeer;
+use Rocketeer\Services\TasksHandler;
+
+use Rocketeer\Plugins\Wordpress\Services;
+use Rocketeer\Plugins\Wordpress\Tasks;
+
 
 class RocketeerWordpress extends AbstractPlugin
 {
+    /**
+     * Constructor
+     */
+    public function __construct (Container $app)
+    {
+        parent::__construct($app);
+
+        // define config folder, it will be available through $this->config('rocketeer-wordpress::config')
+        $this->configurationFolder = __DIR__.'/../config';
+
+        // include all binaries
+        foreach (glob(__DIR__."/Binaries/*.php") as $filename) {
+            require_once $filename;
+        }
+
+        // add tasks
+        Rocketeer::add('Rocketeer\Plugins\Wordpress\Tasks\WpDbPush');
+        Rocketeer::add('Rocketeer\Plugins\Wordpress\Tasks\WpUploadsPush');
+        Rocketeer::add('Rocketeer\Plugins\Wordpress\Tasks\WpUploadsPull');
+    }
+
     /**
      * Bind additional classes to the app container
      */
     public function register(Container $app)
     {
+        // bind sync handler
+        $app->bind('syncHandler', function ($app) { 
+            return new Services\SyncHandler($app);
+        });
+
         return $app;
     }
 
@@ -21,6 +52,6 @@ class RocketeerWordpress extends AbstractPlugin
      */
     public function onQueue(TasksHandler $queue)
     {
-
+        // ...
     }
 }
